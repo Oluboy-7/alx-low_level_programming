e5c5f27b879779ed1aee56bf5c9d24b5cf3aa690
Ed <oluboyy@gmail.com>
2022-06-05 07:33:35 -0700

    task4

---
+++ 0x00-hello_world/4puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+ * main - prints the size of various types
+ * Return: Always 0 (Success)
+ *  **/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzz
+le");
+	        return(0);
+}
